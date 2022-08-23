10f376652845a3a7a593fe4446c97e26a9694e72
davidlp90 <davidivan.eda@gmail.com>
2022-08-23 10:55:29 -0300

    Update script.js

--- Proyecto02-flexbox/public/js/script.js
+++ Proyecto02-flexbox/public/js/script.js
@@ -1,6 +1,10 @@
 let button = document.getElementById('#tirate-unpaso');
 
+var xX_mIpRiMer_VarIable__Xx = 'hOLA MUnDe quE oNda'
 
 function tirateUnPaso() {
     alert('Sin JavaScript no hay Paraiso')
 }
+
+
+
