8d15a05d1bf8ed27cdfd03d79cdeca09c6f833e9
davidlp90 <davidivan.eda@gmail.com>
2022-10-13 19:22:48 -0300

    cambios

--- Proyecto03/public/main.js
+++ Proyecto03/public/main.js
@@ -1,3 +1,5 @@
+//VARIABLES
+
 let firstSection = document.querySelector('#firstsection')
 let itemName = document.querySelector('#name')
 let itemDescription = document.querySelector('#description')
@@ -8,9 +10,11 @@ let itemDivContainer = document.querySelector('#items')
 //BOTONES
 
 let addButton = document.querySelector('#additem').addEventListener('click', function () {
-  document.querySelector('#titulo-main').style.display = 'none'
+
+
   document.querySelector('#firstsection').style.display = 'none'
   document.querySelector('#menu').style.display = 'block'
+
 
 })
 
@@ -22,35 +26,54 @@ let backButton = document.querySelector('#back').addEventListener('click', funct
 let addToListButton = document.querySelector('#add').addEventListener('click', function () {
 
   addCardToMainScreen()
+  document.querySelector('#titulo-main').style.display = 'none'
   document.querySelector('#menu').style.display = 'none'
   document.querySelector('#firstsection').style.display = 'block'
 })
 
 let resetButton = document.querySelector('#reset').addEventListener('click', function () {
-  console.log('anda')
+  location.reload()
+})
+
+let closeAlertButton = document.querySelector('#close-alert').addEventListener('click', function () {
+  document.querySelector('#empty-alert').style.display = 'none'
+  document.querySelector('#menu').style.display = 'block'
 })
 
 //FUNCIONES
 
-function addCardToMainScreen() {
+function addCardToMainScreen(event) {
   let name = itemName.value;
   let icon = itemImage.value;
   let description = itemDescription.value;
 
   if (name != '' && icon != '0') {
     addCard(name, icon, description)
+    resetForm()
   } else {
-    alert('Tenes campos vacios!')
+    showAlert()
+    event.preventDefault();
   }
 }
 
+function showAlert() {
+  document.querySelector('#empty-alert').style.display = 'block'
+  document.querySelector('#menu').style.display = 'none'
+}
+
+function resetForm() {
+  itemName.value = '';
+  itemImage.value = '0';
+  itemDescription.value = '';
+}
 
 function addCard(nameList, iconList, descriptionList) {
 
   let itemFinal = `<div class="card mb-3" style="max-width: 540px; justify-content: center; display: inline-block;">
 <div class="row g-0">
   <div class="col-md-4">
-    <img src="${iconList}" class="img-fluid rounded-start img" alt="...">
+    <img style='width: 50%;
+    height: 100%;' src="${iconList}" class="img-fluid rounded-start img" alt="...">
   </div>
   <div class="col-md-8">
     <div class="card-body">
